957de0c10ae70113c9050c4dba04591a3d7ef8de
Pavel Druzhinin <p.druzhinin@corp.mail.ru>
2020-02-10 18:07:59 +0300

    Init React with structure just before redux

--- leadmanager/settings.py
+++ leadmanager/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'leads',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
